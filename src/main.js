e137032723cfeffef3632a0dab3f05ce018f841a
fulaytar <fulaytar2405@gmail.com>
2024-02-16 01:23:02 +0200

    не працює зміна інпута у модалці

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function createObjToDo(textDone) {
     return {
         id: Date.now(),
         status: 'todo',
-        textDone: textDone
+        textDone: textDone,
     }
 }
 
@@ -32,12 +32,12 @@ function createItem({ id, status, textDone }) {
 //додаю об'єкт
 function addItem() {
     const value = input.value.trim();
-    
+    input.value = "";
     if (!value) {
         alert("Please enter input");
         return;
     }
-    input.value = "";
+    
 
     const ObjToDo = createObjToDo(value);
 
@@ -57,16 +57,54 @@ function initList() {
     list.innerHTML = data.map(createItem).join('');
 }
 
+
+//тут міняємо класи todo
+list.addEventListener('click', openModal)
+
+let instance = null;
+
+function openModal(event) {
+    if (event.target.classList.contains("btn-update")) {
 //madalka
 instance = basicLightbox.create(
-    `<div class="modal-container"><button type="button" class="btn-close-modal">X</button><input type="text" class="input-modal"/><button type="button" class="btn-update-modal" id="${e.target.parentNode.id}">Update todo</button></div>`,
+    `<div class="modal-container">
+    <button type="button" class="btn-close-modal">X</button>
+    <input type="text" class="input-modal"/>
+    <button type="button" class="btn-update-modal" id="${event.target.parentNode.id}">Update todo</button>
+    </div>`,
     {
     closable: false,
     }
-); 
-  
-//тут міняємо класи todo
-/* list.addEventListener('click', updateItem) */
+    ); 
+        instance.show();
+
+    //ставлю події на модальне окно
+
+const modalInput=document.querySelector('.input-modal')
+const modalButton=document.querySelector('.btn-update-modal')
+const closeButton=document.querySelector('.btn-close-modal')
+
+    closeButton.addEventListener('click', () => instance.close());
+    modalButton.addEventListener('click', updateItem);
+    function updateItem(event) {
+        const input = modalInput.value.trim();
+        console.log(input);
+        if (!input) {
+            return;
+        }
+        const data = JSON.parse(localStorage.getItem(LOCAL_TODO));
+        const updData = data.map(obj => {
+        if (obj.id === +event.target.id) {
+            obj.textDone = input;
+        }
+        return obj;
+    })
+        localStorage.setItem(LOCAL_TODO, JSON.stringify(updData));
+        instance.close();
+    }
+
+    }
+}
 
 
 
